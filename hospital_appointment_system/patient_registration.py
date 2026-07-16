b6927bf1da95a92a4ddc90956194ff81a64d0568
SRI RAM <sriram@SRIs-MacBook-Air.local>
2026-07-16 15:25:43 +0530

    case study 2 modify existing file

--- hospital_appointment_system/patient_registration.py
+++ hospital_appointment_system/patient_registration.py
@@ -6,3 +6,6 @@ def register_patient(name, age, phone):
     print(f"Registered {name}, Age: {age}, Phone: {phone}")
 
 register_patient("Alice", 30, "555-1234")
+
+#Modifying the file 
+print(modified the file for case study 2)
